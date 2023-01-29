e6751eb98c4ba0b08c9a2a72a9eb831c92339d8f
humbertodrc <humbertorivero9@gmail.com>
2023-01-29 00:14:52 -0300

    Feat form register

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import React from "react";
 
 export default function Register() {
-  return (
-    <div>Register</div>
-  )
+	return (
+		<>
+			<h1 className="text-4xl font-black">Crea tu cuenta</h1>
+			<p>Crea tu cuenta llenando el formulario</p>
+
+			<div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+				<form>
+					<div className="mb-4">
+            <label
+              htmlFor="name"
+              className="text-slate-800"
+            >
+							Nombre:
+						</label>
+            <input
+              type="text"
+              id="name"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="name"
+              placeholder="Tu Nombre"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="text-slate-800"
+            >
+							Email:
+						</label>
+            <input
+              type="email"
+              id="email"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="email"
+              placeholder="Tu Email"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="text-slate-800"
+            >
+							Password:
+						</label>
+            <input
+              type="password"
+              id="password"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="password"
+              placeholder="Tu Password"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password_confirmation"
+              className="text-slate-800"
+            >
+							Repetir Password:
+						</label>
+            <input
+              type="password"
+              id="password_confirmation"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="password_confirmation"
+              placeholder="Repetir Password"
+            />
+            <input
+              type="submit"
+              value="Crear Cuenta"
+              className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+            />
+					</div>
+				</form>
+			</div>
+		</>
+	);
 }
