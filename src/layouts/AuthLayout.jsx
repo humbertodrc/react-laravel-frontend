a73a1bca2e0cd5f242a42836a32902234604e2ab
humbertodrc <humbertorivero9@gmail.com>
2023-01-28 23:24:00 -0300

    Feat logo and main content in auth layout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,10 +1,12 @@
-import {Outlet} from 'react-router-dom';
+import {Outlet} from "react-router-dom";
 
 export default function AuthLayout() {
-  return (
-    <div>
-      AuthLayout
-      <Outlet />
-    </div>
-  )
+	return (
+		<main className="max-w-4xl mx-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center">
+			<img src="../img/logo.svg" alt="logo" className="max-w-xs" />
+			<div className="p-10 w-full">
+				<Outlet />
+			</div>
+		</main>
+	);
 }
