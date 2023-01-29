34e3f44cb697e723493317fae196b261d5e6a341
humbertodrc <humbertorivero9@gmail.com>
2023-01-28 23:14:22 -0300

    Feat layout Login and layout register

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,6 +1,10 @@
+import {Outlet} from 'react-router-dom'
 
 export default function Layout() {
   return (
-    <div>Layout</div>
+    <div>
+      Layout
+      <Outlet />
+    </div>
   )
 }
--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,34 @@
 import { createBrowserRouter } from 'react-router-dom';
+import AuthLayout from './layouts/AuthLayout';
 import Layout from './layouts/Layout';
+import Inicio from './views/Inicio';
+import Login from './views/Login';
+import Register from './views/Register';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element : <Layout />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Inicio />
+      }
+    ]
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/auth/login',
+        element: <Login />
+      },
+      {
+        path: '/auth/register',
+        element: <Register />
+      }
+    ]
   }
 ])
 
