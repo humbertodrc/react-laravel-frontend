93dea8866d7d86e4330f2e22513695d3099ea4aa
humbertodrc <humbertorivero9@gmail.com>
2023-01-29 13:37:00 -0300

    Feat main layout

---
+++ src/components/Summary.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Summary() {
+  return (
+    <div className='w-72'>Summary</div>
+  )
+}
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,15 @@
 import {Outlet} from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
+import Summary from '../components/Summary'
 
 export default function Layout() {
   return (
-    <div>
-      Layout
-      <Outlet />
+    <div className='md:flex'>
+      <Sidebar />
+      <main className='flex-1'>
+        <Outlet />
+      </main>
+      <Summary />
     </div>
   )
 }
