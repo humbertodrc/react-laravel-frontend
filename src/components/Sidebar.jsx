35ec134854588a37ec836b10f6a473ff51305d8d
humbertodrc <humbertorivero9@gmail.com>
2023-01-29 13:59:12 -0300

    Feat show sidebar and categories

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { categorias } from '../data/categorias';
 
 export default function Sidebar() {
-  return (
-    <div className='md:w-72'>Sidebar</div>
-  )
+	return (
+		<aside className="md:w-72">
+			<div className="p-4">
+				<img src="img/logo.svg" alt="" className="w-40" />
+      </div>
+      <div className='mt-10'>
+        {categorias.map((categoria) => (
+          <p>{categoria.nombre}</p>
+          ))}
+      </div>
+		</aside>
+	);
 }
