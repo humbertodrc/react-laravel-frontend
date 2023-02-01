9c6ce9e782224154108dfad37391b84b8a9efa34
humbertodrc <humbertorivero9@gmail.com>
2023-02-01 20:44:12 -0300

    Add data for component product

---
+++ src/components/Product.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Product({ producto }) {
+  
+  console.log(producto);
+
+  return (
+    <div>Product</div>
+  )
+}
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,6 +1,18 @@
+import Product from '../components/Product'
+import { productos } from '../data/productos'
 
 export default function Inicio() {
+
   return (
-    <div>Inicio</div>
+    <>
+      <h1 className='text-4xl font-black'>Inicio</h1>
+      <p className='text-2xl my-10'>Elige y personaliza tu pedido a continuación</p>
+
+      <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+        {productos.map((producto) => (
+            <Product key={producto.id} producto={producto} />
+          ))}
+      </div>
+    </>
   )
 }
