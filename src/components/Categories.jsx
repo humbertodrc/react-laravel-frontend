f28938dc4a7f4da53ef6df8f5a21a0461a54fab7
humbertodrc <humbertorivero9@gmail.com>
2023-01-31 22:02:17 -0300

    Feat styles sidebar

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-
 export default function Categories({categoria}) {
-	const {icono, nombre} = categoria;
+	const {icono, nombre, id} = categoria;
 
 	return (
-		<div>
-			{nombre} {icono}
+		<div className="flex items-center gap-4 border w-full p-3 hover:bg-amber-400 cursor-pointer">
+			<img
+				src={`/img/icono_${icono}.svg`}
+				alt={`imagen icono ${nombre}`}
+				className="w-12"
+			/>
+			<p className="text-lg font-bold cursor-pointer truncate">{nombre}</p>
 		</div>
 	);
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,25 @@
-import { categorias } from '../data/categorias';
-import Categories from './Categories';
+import {categorias} from "../data/categorias";
+import Categories from "./Categories";
 
 export default function Sidebar() {
 	return (
 		<aside className="md:w-72">
 			<div className="p-4">
 				<img src="img/logo.svg" alt="" className="w-40" />
-      </div>
-      <div className='mt-10'>
-        {categorias.map((categoria) => (
-          <Categories key={categoria.id} categoria={categoria} />
-          ))}
-      </div>
+			</div>
+			<div className="mt-10">
+				{categorias.map((categoria) => (
+					<Categories key={categoria.id} categoria={categoria} />
+				))}
+			</div>
+			<div className="my-5 px-5">
+				<button
+					type="button"
+					className="text-center bg-red-500 w-full p-3 font-bold text-white truncate"
+				>
+					Cancelar Orden
+				</button>
+			</div>
 		</aside>
 	);
 }
