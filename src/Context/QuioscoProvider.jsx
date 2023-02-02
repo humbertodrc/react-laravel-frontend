3bdd28d43a27eaa0864749d7b7399177c09bc7e8
humbertodrc <humbertorivero9@gmail.com>
2023-02-01 21:39:50 -0300

    Add context provider

---
+++ src/Context/QuioscoProvider.jsx
@@ -0,0 +1,22 @@
+import { createContext } from 'react'
+
+const QuioscoContext = createContext()
+
+const QuioscoProvider = ({ children }) => {
+  
+  return (
+    <QuioscoContext.Provider
+      value={{
+      
+      }}
+    >
+      {children}
+    </QuioscoContext.Provider>
+    )
+}
+
+export {
+  QuioscoProvider
+}
+
+export default QuioscoContext;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import router from './router'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {RouterProvider} from "react-router-dom";
+import {QuioscoProvider} from "./Context/QuioscoProvider";
+import "./index.css";
+import router from "./router";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<QuioscoProvider>
+			<RouterProvider router={router} />
+		</QuioscoProvider>
+	</React.StrictMode>
+);
