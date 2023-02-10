fcdfe16d06764c6d2dea42575fab0248aeba12b7
humbertodrc <humbertorivero9@gmail.com>
2023-02-10 20:42:18 -0300

    Add react-modal

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,21 +1,40 @@
-import {Outlet} from 'react-router-dom'
-import Sidebar from '../components/Sidebar'
-import Summary from '../components/Summary'
-import useQuiosco from '../hooks/useQuiosco'
+import {Outlet} from "react-router-dom";
+import Modal from "react-modal";
+import Sidebar from "../components/Sidebar";
+import Summary from "../components/Summary";
+import useQuiosco from "../hooks/useQuiosco";
+
+const customStyles = {
+	content: {
+		top: "50%",
+		left: "50%",
+		right: "auto",
+		bottom: "auto",
+		marginRight: "-50%",
+		transform: "translate(-50%, -50%)",
+	},
+};
 
 export default function Layout() {
+	const {modal, handleClickModal} = useQuiosco();
 
-  const { modal } = useQuiosco()
-  
-  console.log(modal)
-
-  return (
-    <div className='md:flex'>
-      <Sidebar />
-      <main className='flex-1 h-screen overflow-y-scroll bg-gray-100 p-4'>
-        <Outlet />
-      </main>
-      <Summary />
-    </div>
-  )
+	return (
+		<>
+			<div className="md:flex">
+				<Sidebar />
+				<main className="flex-1 h-screen overflow-y-scroll bg-gray-100 p-4">
+					<Outlet />
+				</main>
+				<Summary />
+			</div>
+      {modal &&
+        <Modal
+          isOpen={modal}
+          style={customStyles}
+        >
+          <button onClick={handleClickModal}>Cerrar</button>
+        </Modal>
+      }
+		</>
+	);
 }
