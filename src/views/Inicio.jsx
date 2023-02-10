ed822224abe79fe3bb39a05e4a502b8e66cbdad2
humbertodrc <humbertorivero9@gmail.com>
2023-02-10 20:19:39 -0300

    Feat filter product by category

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,21 +1,26 @@
-import Product from '../components/Product'
-import { productos } from '../data/productos'
-import useQuiosco from '../hooks/useQuiosco'
+import Product from "../components/Product";
+import {productos as data} from "../data/productos";
+import useQuiosco from "../hooks/useQuiosco";
 
 export default function Inicio() {
+	const {categoriaActual} = useQuiosco();
 
-  const { categoriaActual } = useQuiosco();
+	const productos = data.filter(
+		(producto) => producto.categoria_id === categoriaActual.id
+	);
 
-  return (
-    <>
-      <h1 className='text-4xl font-black'>{categoriaActual.nombre}</h1>
-      <p className='text-2xl my-10'>Elige y personaliza tu pedido a continuación</p>
+	return (
+		<>
+			<h1 className="text-4xl font-black">{categoriaActual.nombre}</h1>
+			<p className="text-2xl my-10">
+				Elige y personaliza tu pedido a continuación
+			</p>
 
-      <div className='grid gap-4 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'>
-        {productos.map((producto) => (
-            <Product key={producto.id} producto={producto} />
-          ))}
-      </div>
-    </>
-  )
+			<div className="grid gap-4 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+				{productos.map((producto) => (
+					<Product key={producto.id} producto={producto} />
+				))}
+			</div>
+		</>
+	);
 }
