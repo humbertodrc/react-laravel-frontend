7962a6c7b54f4732557b8a0c4f84a30bcfbb1702
humbertodrc <humbertorivero9@gmail.com>
2023-02-10 21:46:37 -0300

    Add number of productos in modal

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,11 +1,15 @@
+import {useState} from "react";
 import {formatearDinero} from "../helpers";
 import useQuiosco from "../hooks/useQuiosco";
 
 export default function ModalProducto() {
 	const {producto, handleClickModal} = useQuiosco();
 	const {nombre, imagen, precio} = producto;
+
+	const [cantidad, setCantidad] = useState(1);
+
 	return (
-		<div className="md:flex gap-10">
+		<div className="md:flex ite gap-10">
 			<div className="md:w-1/3">
 				<img
 					className="w-full"
@@ -36,7 +40,59 @@ export default function ModalProducto() {
 				<p className="mt-5 font-black text-5xl text-amber-500">
 					{formatearDinero(precio)}
 				</p>
-				<button className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded">Añadir al pedido</button>
+				<div className="flex gap-4 mt-5">
+					<button
+						type="button"
+						onClick={() => {
+							if (cantidad <= 1) return;
+							setCantidad(cantidad - 1);
+						}}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							className="w-6 h-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+					</button>
+					<p className="text-3xl">{cantidad}</p>
+					<button
+						type="button"
+						onClick={() => {
+							if (cantidad >= 5) return;
+							setCantidad(cantidad + 1);
+						}}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							className="w-6 h-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+					</button>
+				</div>
+				<button
+					type="button"
+					className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded"
+				>
+					Añadir al pedido
+				</button>
 			</div>
 		</div>
 	);
